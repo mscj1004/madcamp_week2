1820a600f78eb385c756228feda24af7e4824964
ssook1222 <chlthr1222@naver.com>
2022-07-07 18:37:53 +0900

    UPDATE : 현재 진행상황 ADD

---
+++ app/src/main/java/com/example/madcamp_week2/LoginActivity.java
@@ -0,0 +1,21 @@
+package com.example.madcamp_week2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private Button next;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+
+    }
+
+}
