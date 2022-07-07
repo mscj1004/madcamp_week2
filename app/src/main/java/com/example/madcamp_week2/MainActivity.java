c2e0cbee4bb105e456294dc7a1620a1aeeefb233
mscj1004 <mscj1004@kaist.ac.kr>
2022-07-07 22:10:52 +0900

    add fragment

--- app/src/main/java/com/example/madcamp_week2/MainActivity.java
+++ app/src/main/java/com/example/madcamp_week2/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.madcamp_week2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+
+import com.google.android.material.tabs.TabLayout;
+import com.example.madcamp_week2.SectionsPagerAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +16,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+        ViewPager viewPager = findViewById(R.id.viewpager);
+        viewPager.setAdapter(sectionsPagerAdapter);
+        TabLayout tabs = findViewById(R.id.tabLayout);
+        tabs.setupWithViewPager(viewPager);
+        tabs.getTabAt(0).setIcon(R.drawable.weather);
+        tabs.getTabAt(1).setIcon(R.drawable.home);
+        tabs.getTabAt(2).setIcon(R.drawable.hanger);
+        tabs.getTabAt(3).setIcon(R.drawable.heart);
     }
 
 }
---
+++ app/src/main/java/com/example/madcamp_week2/SectionsPagerAdapter.java
@@ -0,0 +1,58 @@
+package com.example.madcamp_week2;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+
+    private static final int[] TAB_TITLES = new int[]{R.string.tab_text_1, R.string.tab_text_2, R.string.tab_text_3, R.string.tab_text_4};
+    private final Context mContext;
+
+    public SectionsPagerAdapter(Context context, FragmentManager fm){
+        super(fm);
+        mContext = context;
+
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        Fragment fragment = null;
+        switch(position){
+            case 0:
+                fragment = new Fragment_1();
+                break;
+
+            case 1:
+                fragment = new Fragment_2();
+                break;
+
+            case 2:
+                fragment = new Fragment_3();
+                break;
+
+            case 3:
+                fragment = new Fragment_4();
+                break;
+        }
+        return fragment;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position){
+        return mContext.getResources().getString(TAB_TITLES[position]);
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
