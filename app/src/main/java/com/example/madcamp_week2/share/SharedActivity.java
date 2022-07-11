fe373e5231efb151f5b79f458e6376fc22ce2751
ssook1222 <chlthr1222@naver.com>
2022-07-11 18:51:06 +0900

    Merge branch 'refactor' of /Users/choisook/madcamp_week2/madcamp_week2 with conflicts.

---
+++ app/src/main/java/com/example/madcamp_week2/share/SharedActivity.java
@@ -0,0 +1,16 @@
+package com.example.madcamp_week2.share;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.madcamp_week2.R;
+
+public class SharedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shared);
+    }
+}
